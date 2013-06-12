f989b7661dabde4c9963c74088d72cf322e64b0c
sidra.bhatti@vibes.com <sidra.bhatti@vibes.com>
2013-06-11 19:08:53 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Extracredit::Application.config.secret_token = '1ad5274f3eae684933073f4f67961ec3357e1d7ff3c94480c69771523a4c408eac372abc77bab7e86ed9173b56496107df6995375f4482626575d627dd499112'
